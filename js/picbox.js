8e99099270123387affdffa114d9962d6449d542
randomizedfluff <randomizedfluff@googlemail.com>
2010-02-05 22:32:09 +0000

    Controls now disappear even if mouse is never moved
    git-svn-id: http://picboxjs.googlecode.com/svn/trunk@32 fcdbf19e-f2a5-11de-9a6a-9d1f184cc70b

--- js/picbox.js
+++ js/picbox.js
@@ -87,6 +87,7 @@
 		
 		$(overlay).css("opacity", 0).fadeTo(options.overlayFadeDuration, options.overlayOpacity);
 		$(bottom).css("display", "");
+		mouseMove(); // So controls dissapear if even if mouse is never moved
 		position();
 		setup(1);
 
